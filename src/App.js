4fadb80aa2ace7a970accce29adaf0db8666aa96
Hakki Ogretmen <haogretm@microsoft.com>
2020-07-06 19:45:34 +0300

    welcome text edited

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const value = 'wathing';
+  return <div>Thank you for {value}</div>;
 }
 
 export default App;
